64665df03a767533ba882ada9d6f82fd15502c6f
Nimit Gupta <guptanimit062@gmail.com>
2025-10-31 13:39:42 +0530

    Added new Links for deployment

--- server/app.py
+++ server/app.py
@@ -28,11 +28,11 @@ except:
     pass
 
 app = FastAPI(title="Multi-Modal Sentiment Analyzer API with LLM")
-
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=FRONTEND_URL,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
